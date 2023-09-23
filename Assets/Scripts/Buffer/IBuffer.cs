e0ce5edc7ba1b1b0c9330f70bc501d672f78f084
Cj-bc <cj.bc-sd@outlook.jp>
2023-09-24 01:05:49 +0900

    Add: Scripts/Buffer/IBuffer> point related properties

--- Assets/Scripts/Buffer/IBuffer.cs
+++ Assets/Scripts/Buffer/IBuffer.cs
@@ -3,6 +3,12 @@
 /// </summary>
 public interface IBuffer
 {
+    /// <summary>Current point (Cursor position in the buffer)</summary>
+    public int point {get; set; }
+
+    /// <summary>End of buffer point</summary>
+    public int endPoint {get; }
+
     /// <summary> Returns full text in buffer with rich text markup</summary>
     public string Contents(); 
 }

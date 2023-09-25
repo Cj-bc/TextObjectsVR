57f26dd9ea3aea0cdfa522cebd7d09c600266a88
Cj-bc <cj.bc-sd@outlook.jp>
2023-09-25 18:07:27 +0900

    Change: Scripts/Buffer/BufferUI> use triggerEvent to response to click event
    interactable seems not to be the way to interact with uGUI gameObjects...

--- Assets/Scripts/Buffer/BufferUI.cs
+++ Assets/Scripts/Buffer/BufferUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using TMPro;
+using UnityEngine.EventSystems;
 
 /// <summary>Visualize Buffer</summary>
 [RequireComponent(typeof(TextMeshProUGUI))]
@@ -49,6 +50,30 @@ public class BufferUI : XRSimpleInteractable
 	    Debug.Log($"Intersected at ${idx}, which is ${text.text[idx]}");
 	} else {
 	    Debug.Log("Failed to get hit point");
+	}
+    }
+
+    public void pointerClick(BaseEventData d) {
+	var pointerEv = d as PointerEventData;
+	if (pointerEv == null) {
+	    Debug.Log("Expected pointer click event, but failed to convert.");
+	    return;
+	}
+
+	// intersecting point in <c>Camera.main</c> screen space
+	var intersectPointInScreenSpace = Camera.main.WorldToScreenPoint(pointerEv.pointerPressRaycast.worldPosition);
+	var idx = TMP_TextUtilities.FindIntersectingCharacter(text, intersectPointInScreenSpace, Camera.main, false);
+	if (idx != -1) {
+	    Debug.Log($"Intersected at {idx}, which is {text.text[idx]}");
+
+	    // find word
+	    var word = new Word();
+	    var beg = word.findBeginning(buf, idx);
+	    var end = word.findEnd(buf, idx);
+
+	    if (beg != null && end != null) {
+		Debug.Log($"Selected word: {buf.Contents().Substring(beg ?? 0,(end ?? 0)-(beg ?? 0))}");
+	    }
 	}
     }
 }
